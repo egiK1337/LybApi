d6e85705821e4b3a584708db93276b92f810466e
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2023-05-15 15:59:47 +0300

    add serviceLayer.abstractions with DTO

---
+++ ServiceLayer.Abstractions/DTO/Author/AuthorsListDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Abstractions.DTO.Author
+{
+    internal class AuthorsListDto
+    {
+    }
+}
