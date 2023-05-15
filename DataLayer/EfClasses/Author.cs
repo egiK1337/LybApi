cc9d1926751988c33b8b0b6326d27b3adf70d6af
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2023-05-15 15:14:46 +0300

    add DataLayer

---
+++ DataLayer/EfClasses/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer.EfClasses;
+
+public class Author
+{
+    public Author()
+    {
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string WebUrl { get; set; } = string.Empty;
+
+    ICollection<BookAuthor> Books { get; set; }
+}
